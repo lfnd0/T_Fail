27e2381b73278c1e4869818e31ee0a60586b69e7
luizfernando <luiz-fsilva37@hotmail.com>
2019-08-28 18:28:37 -0300

    Adição de professor-turma :heavy_check_mark:

--- turmas/models.py
+++ turmas/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-from usuarios.models import User
+from usuarios.models import Professor
 
 class Turma(models.Model):
-    professor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='turmas')
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='turmas')
     nome = models.CharField(max_length=100)
     
     def __str__(self):
