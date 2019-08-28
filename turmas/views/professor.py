2b9f621d5049d79f6993f35746a7216217c5c3c1
luizfernando <luiz-fsilva37@hotmail.com>
2019-08-28 18:27:13 -0300

    Adição professor-turma :heavy_check_mark:

--- turmas/views/professor.py
+++ turmas/views/professor.py
@@ -11,22 +11,22 @@ from ..decorators import professor_required
 @method_decorator([login_required, professor_required], name='dispatch')
 class TurmaListView(ListView):
     model = Turma
-    ordering = ('name', )
+    ordering = ('nome', )
     context_object_name = 'turmas'
     template_name = 'professor/professor_home.html'
 
     def get_queryset(self):
-        queryset = self.request.user.turmas
+        queryset = self.request.user.professor.turmas
         return queryset
 
 @method_decorator([login_required, professor_required], name='dispatch')
 class TurmaCreateView(CreateView):
     model = Turma
-    fields = ('nome')
+    fields = ('nome', )
     template_name = 'professor/adicionar_turma.html'
 
     def form_valid(self, form):
         turma = form.save(commit=False)
-        turma.professor = self.request.user
+        turma.professor = self.request.user.professor
         turma.save()
         return redirect('professor_home')
