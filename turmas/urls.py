256db165acfcbcd0aaeb1194cf208c001bfaa5a6
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-09-02 11:41:46 -0300

    Refatoração urls :heavy_check_mark:

--- turmas/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-
-from .views import professor
-
-urlpatterns = [
-    path('professor', professor.TurmaListView.as_view(), name='professor_home'),
-    path('professor/adicionar', professor.TurmaCreateView.as_view(), name='adicionar_turma')
-]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
-from . import views
+
+from .views import estudantes, professores, usuarios
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('usuarios/', include('django.contrib.auth.urls')),
-    path('usuarios/signup/', views.SignUpView.as_view(), name='signup'),
-    path('usuarios/signup/estudante/', views.EstudanteSignUpView.as_view(), name='estudante_signup'),
-    path('usuarios/signup/professor/', views.ProfessorSignUpView.as_view(), name='professor_signup')
+    path('', usuarios.home, name='home'),
+
+    path('professores/', include(([
+        path('', professores.TurmaListView.as_view(), name='listar_turmas'),
+        path('turma/adicionar/', professores.TurmaCreateView.as_view(), name='adicionar_turma')
+    ], 'usuario'), namespace='professores'))
 ]
