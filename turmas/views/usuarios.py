589d505f8db0918abd6f9601835f36a6464992c8
luizfernando <luiz-fsilva37@hotmail.com>
2019-09-18 11:33:34 -0300

    Adição view-turma :heavy_check__mark:

--- turmas/views/usuarios.py
+++ turmas/views/usuarios.py
@@ -5,6 +5,9 @@ class SignUpView(TemplateView):
     template_name = 'registration/signup.html'
 
 def home(request):
+    return render(request, 'usuario/home.html')
+
+def turma(request):
     if request.user.is_authenticated:
         if request.user.is_professor:
             return redirect('professores:listar_turmas_professor')
