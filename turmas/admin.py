5d2740de11e38ce494d09d3510b1cfcfb85eb969
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-10-21 12:19:50 -0300

    Adição novos models em admin

--- turmas/admin.py
+++ turmas/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import User, Estudante, Professor, Turma
+from .models import User, Estudante, Professor, Turma, Atividade, Problema, Submissao
 
 admin.site.register(User)
 admin.site.register(Estudante)
 admin.site.register(Professor)
 admin.site.register(Turma)
+admin.site.register(Problema)
+admin.site.register(Atividade)
+admin.site.register(Submissao)
