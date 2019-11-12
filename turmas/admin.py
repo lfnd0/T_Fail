fd087c201093ce74e99083622c1b6d1bf178869e
Luiz Fernando <luiz-fsilva37@hotmail.com>
2019-11-12 11:51:33 -0300

    Registro admin model Avaliacao

--- turmas/admin.py
+++ turmas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import User, Estudante, Professor, Turma, Atividade, Problema, Submissao
+from .models import User, Estudante, Professor, Turma, Atividade, Problema, Submissao, Avaliacao
 
 admin.site.register(User)
 admin.site.register(Estudante)
@@ -9,3 +9,4 @@ admin.site.register(Turma)
 admin.site.register(Atividade)
 admin.site.register(Problema)
 admin.site.register(Submissao)
+admin.site.register(Avaliacao)
